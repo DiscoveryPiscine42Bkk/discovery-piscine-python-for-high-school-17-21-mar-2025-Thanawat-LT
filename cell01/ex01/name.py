e38f56267a38adcb51e2bbeb2194581248f4eb4d
Thanawat-LT <phuckjira.c99@gmail.com>
2025-03-17 20:46:57 +0700

    cell1.1

---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+variable_first_name = "Thanawat"
+variable_last_name = "Ratthong"
+variable_whole_name = variable_first_name + " " + variable_last_name  # Add " " between names
+print(variable_whole_name)
