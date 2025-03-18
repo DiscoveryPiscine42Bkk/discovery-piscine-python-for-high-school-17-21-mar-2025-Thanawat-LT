70a5cf721fb720ac46d236605b14aaa5cfc4f468
Thanawat-LT <phuckjira.c99@gmail.com>
2025-03-18 15:07:29 +0700

    cell 3.0

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-a=int(input("Enter a number less than 25"))
-if a > 25:
+X=int(input("Enter a number less than 25"))
+if X > 25:
     print("Error")
-elif a < 25:
-    print(a)
-while a < 26:
-    print(f"Inside the loop, my variable is  {a}")
-    a += 1
+elif X < 25:
+    print(X)
+while X < 26:
+    print(f"Inside the loop, my variable is  {X}")
+    X += 1
