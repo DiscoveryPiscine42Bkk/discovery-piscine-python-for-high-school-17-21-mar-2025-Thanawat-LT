c99d0774a1a09abf8b6ef050ea73de00a0796e7a
Thanawat-LT <phuckjira.c99@gmail.com>
2025-03-19 12:17:35 +0700

    cell 4.2

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+a=int(input("Enter the first number"))
+b=int(input("Enter the second number"))
+print('Thank you!')
+print(f'''{a} + {b}  = {a+b}''')
+print(f'''{a} - {b}  = {a-b}''')
+print(f'''{a} / {b}  = {a/b}''')
+print(f'''{a} * {b}  = {a*b}''')
