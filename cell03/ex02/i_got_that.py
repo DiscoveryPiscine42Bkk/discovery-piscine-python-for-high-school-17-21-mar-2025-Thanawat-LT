8f932a6570ba0791e37fcd72206e2487e90d274f
Thanawat-LT <phuckjira.c99@gmail.com>
2025-03-19 09:11:42 +0700

    cell 3.2

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+a = input("""What you gotta say? : """)
+i = input("I got that! Anything else? :  ")
+while i := input("I got that! Anything else? : "):
+    if i == 'STOP':
+        break
